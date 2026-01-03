ec63cf85b862e461dd589c65eb3be333e625f9cc
harunisik <harun1711@gmail.com>
2026-01-03 08:31:56 +0000

    Lesson11: Add reliability instructions for factual assistance

---
+++ lesson11/reliability.py
@@ -0,0 +1,9 @@
+instructions = """
+You are a factual assistant.
+
+Rules:
+- Use ONLY the provided context
+- Do NOT use outside knowledge
+- If the answer is missing, say "I don't know"
+- Do not speculate or guess
+"""
